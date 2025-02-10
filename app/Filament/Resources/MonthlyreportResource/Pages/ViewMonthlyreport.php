2dcb64eff3dec76d68d8a26227f620dbabe01977
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-02-10 17:20:08 +0900

    fix : pdf download

--- app/Filament/Resources/MonthlyreportResource/Pages/ViewMonthlyreport.php
+++ app/Filament/Resources/MonthlyreportResource/Pages/ViewMonthlyreport.php
@@ -20,21 +20,21 @@ class ViewMonthlyreport extends ViewRecord
             Action::make('Export PDF')
                     ->label('Laporan Tugas Harian')
                     ->icon('heroicon-o-folder-arrow-down')
-                    ->url(fn ($record) => route('pdftugasharian', $record->id), '_blank')
+                    ->url(fn ($record) => route('pdftugasharian', $record->id))
                     ->button()
                     ->color('info'),
 
             Action::make('Data Presensi')
                     ->label('Data Presensi')
                     ->icon('heroicon-o-arrow-down-on-square-stack')
-                    ->url(fn ($record) => route('pdfdatapresensi', $record->id), '_blank')
+                    ->url(fn ($record) => route('pdfdatapresensi', $record->id))
                     ->button()
                     ->color('success'),
 
             Action::make('Laporan Presensi')
                     ->label('Laporan Presensi')
                     ->icon('heroicon-o-cloud-arrow-down')
-                    ->url(fn ($record) => route('pdflaporanpresensi', $record->id), '_blank')
+                    ->url(fn ($record) => route('pdflaporanpresensi', $record->id))
                     ->button()
                     ->color('brown'),
 
