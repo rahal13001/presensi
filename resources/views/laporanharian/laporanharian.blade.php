91a6eb66afd92bdebd5a20b469aee9b937c444f2
rahal13001 <rahal13001@mail.unpad.ac.id>
2025-03-06 13:51:55 +0900

    Update : Add daily report display

--- resources/views/laporanharian/laporanharian.blade.php
+++ resources/views/laporanharian/laporanharian.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ asset('img/logoweb.png') }}">
     <title>Laporan Harian</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
