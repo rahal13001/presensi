96149561b070886b94731c1fcb1a8fe618cb0e45
rahal13001 <rahal13001@mail.unpad.ac.id>
2026-02-19 15:57:23 +0900

    fix : update frontend for user login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'api-doc-access' => CheckApiDocAccess::class,
             'check-user-group' => CheckUserGroup::class
         ]);
-        
+
+        $middleware->redirectGuestsTo('/admin/login');
     })
     ->withCommands([
         // AssignCommonEmployeeShifts::class
